6c5e599d83f9036f626ac70105d848fe27a8f7b1
Ruranjo <rubens.rangel.job@gmail.com>
2025-10-04 11:10:13 -0500

    configurar astro.config para GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://ruranjo.github.io',      // 👈 tu usuario de GitHub
+  base: '/astro-juice-landing',           // 👈 nombre exacto de tu repo
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
